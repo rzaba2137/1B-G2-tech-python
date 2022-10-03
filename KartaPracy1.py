087da7e1aeddc4787dfae74862e445c1c7853a8e
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-10-03 11:20:49 +0000

    zrobione zadania 1-3 z kart pracy

---
+++ KartaPracy1.py
@@ -0,0 +1,15 @@
+# zad 1
+
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+# zad 2
+
+# a = int (input())
+# b = int (input())
+# print((a+b) ** 2)
+
+# zad 3
+a, b = int(input()), int(input())
+print((a - b) **3)
