4f917221270f3830564a2b3ed1f5a41926f86311
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-10-05 11:19:54 +0000

    dodana 4 zadania karty pracy 2

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -1,1 +1,26 @@
-#
+# zad 1
+# a = int(input("Podaj liczbe"))
+# if a % 3 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# zad 2 
+# a  = int(input("Zapodaj liczbe"))
+# if a>=100 and a<1000 and a%17==0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# zad 3
+# wiek =int(input("A ile ty masz lat?\n"))
+# if wiek >= 18:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# zad 4
+LIMIT = 20
+waga = int(input("Ile waży ten grozowóz?"))
+if waga=LIMIT:
+    print("A jedź")
