aa1dfb811fd85fba88115879c858d81ce2972aaa
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-10-10 11:19:27 +0000

    dokończono kartę pracy 2.py + ankieta

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -20,7 +20,41 @@
 #     print("NIE")
 
 # zad 4
-LIMIT = 20
-waga = int(input("Ile waży ten grozowóz?"))
-if waga=LIMIT:
-    print("A jedź")
+# LIMIT = 20
+# waga = int(input("Ile waży ten grozowóz?"))
+# if waga=LIMIT:
+#     print("A jedź") 
+
+# ANKIETA
+# a = int(input("Podaj liczby:"))
+# b = int(input("Podaj liczbe:"))
+# if a > b:
+#     print ("TAK")
+# else:
+#     print ("NIE")
+
+# zad 5
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# if a < c < b or b > c > a:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# zad 6
+# a = int(input())
+# p = int(input())
+# if (a**p - a) % p == 0:
+#     print("TAK spełnia MTF")
+# else:
+#     print("NIE spełnia MTF")
+
+# zad 7
+# p = int(input())
+# k = int(input())
+# s = int(input())
+# if p + 3*s >= k:
+#     print("TAK, plaz przeskoczy")
+# else:
+#     print("NIE, zaba nie da rady")
