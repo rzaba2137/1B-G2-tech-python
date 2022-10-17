d6f1f51d19c52602f0e5fcc49481e4a4560284f6
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-10-17 11:20:43 +0000

    zrobiono zadanie 3

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -14,3 +14,24 @@
 #     print(i**3+3, end=" ")
 
 #Zad 2
+# for i in range(105, 1000, 15) :
+#     print (i, end = "")
+
+#PRE 2
+# pętle for liczb trzycyfrowych podzielnych przez 13
+# for i in range(100, 1000):
+#     if i% 13 == 0:
+#         print(i, end = "")
+# pętle for liczb dwucyfrowych parzystych
+# for i in range(10, 100): 
+#     if i % 2, == 0:
+#         print(i, end = "")
+# pętle for potęg cyfr: 0, 1, 4, 9, 16, .. 81
+# for i in range(10):
+#     print(i*i, end = " ")
+
+#zad 3
+n = int(input())
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i)
