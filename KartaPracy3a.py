8ee8e99e11622a498eb8fe855a6b98b27ea8da63
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-10-26 11:20:29 +0000

    zrobione 2 zadania

---
+++ KartaPracy3a.py
@@ -0,0 +1,13 @@
+#zad 1 
+# n = int(input())
+# for i in range(n):
+#     print("*-|", end = "" )
+
+#zad 2
+# n = int(input())
+# for i in range(1, n+1):
+#     print("*" *i)
+#     if i % 2 == 1:
+#         print("||")
+#     else:
+#         print("--", end="")
