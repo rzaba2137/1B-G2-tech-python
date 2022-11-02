065c58fcb588b8c2c50eb8ac5b26dcd7c686ec75
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-11-02 12:20:35 +0000

    zaczęto powtórkę do spr

---
+++ PrzedSpr1.py
@@ -0,0 +1,21 @@
+#zad 1
+# k=int(input())
+# suma=0
+# for i in range(100, 100+k):
+#        suma = suma + i
+# print(suma)
+
+#zad 2
+# n=int(input())
+# a, b = 0, 1
+# suma=0
+# for i in range(n):
+#     a, b = b, a+b
+#     suma = suma + b
+# print(suma)    
+
+# zad 3
+n=int(input())
+suma=0
+for i in range(n):
+    
