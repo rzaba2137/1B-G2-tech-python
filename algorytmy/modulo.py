47de47b593063545890320f6b1b287a8bde2df3b
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-11-30 12:05:52 +0000

    zrobiono nww

--- algorytmy/modulo.py
+++ algorytmy/modulo.py
@@ -1,4 +1,10 @@
-a = int(input())
-b = int(input())
-while b > 0:
-    a, b = b, a % b
+# a = int(input())
+# b = int(input())
+# while b > 0:
+#     a, b = b, a % b
+
+a, b = int(input()), int(input())
+iloczyn = a * b
+a, b = b, a%b
+nwd = a
+print(iloczyn // nwd)
--- algorytmy/odejmowanie.py
+++ algorytmy/odejmowanie.py
@@ -1,5 +1,12 @@
-a, b = int(input()), int(input())
-while a != b:
-    if a > b : a = a - b
-    if b > a : b = b - a
-print(a)
+# a, b = int(input()), int(input())
+# while a != b:
+#     if a > b : a = a - b
+#     if b > a : b = b - a
+# print(a)
+
+# a, b = int(input()), int(input())
+# iloczyn = a * b
+# while a != b:
+#     if a > b : a = a - b
+#     if b > a : b = b - a
+# print(iloczyn // a)
