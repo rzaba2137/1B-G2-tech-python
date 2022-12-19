d7029c5ee4897b00c323482073ab3021b0984954
1B25FS <114579202+rzaba2137@users.noreply.github.com>
2022-12-19 12:19:56 +0000

    zaczęto kartę pracy 6a

---
+++ KartyPracy/KartaPracy6a.py
@@ -0,0 +1,39 @@
+#zad1
+# x = int(input())
+# suma = 0
+# while x > 0:
+#     cyfra = x % 10
+#     suma = suma + cyfra
+#     x = x // 10
+# print(suma)
+
+#zad2
+# a = int(input())
+# flaga = True
+# for i in range(2, a):
+#     if a % i == 0:
+#         flaga = False
+#         break
+# if flaga == True:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+#wersja2
+# a = int(input())
+# for i in range(2, a):
+#     if a % i == 0:
+#         print("NIE")
+#         exit(0)
+# print("TAK")
+
+#wersja3
+# a = int(input())
+# suma = 0
+# for i in range(2, a):
+#     if a % i == 0:
+#         suma = suma + i
+# if suma == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
